18012c9214ba8809ef6daa759d3b627b79cdf956
haticeerdagi <haticeerdagi@gmail.com>
2018-11-26 23:08:05 +0300

    Add files via upload

---
+++ VizeSonrasi_HAFTA1.py
@@ -0,0 +1,10 @@
+#Drawing ellipse
+import matplotlib.pyplot as plt
+a=3
+b=5
+#plot the ellipse
+x=np.linspace(-5.0, 5.0, num=100000)
+y=(b**2*(1-(x**2)/(a**2)))**.5
+plt.plot(x,y)
+plt.plot(x,-y)
+plt.show()
